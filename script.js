a8b28d4f55d3ac6e2eb0f4082288f2d06066eeb5
hufng24 <huyhung0686@gmail.com>
2024-05-29 22:56:20 +0700

    upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Cảm ơn chú đã cho cháu nhaa hehe😽";
+  gif.src =
+    "./z5489212936801_c99dd9cc3ba0a9aa61aa942987328a1f.jpg";
+
+  // Ẩn cả hai nút Yes và No
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+  questionPrompt.style.display = "none"; // Ẩn dòng chữ khi người dùng nhấn vào nút Yes
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
